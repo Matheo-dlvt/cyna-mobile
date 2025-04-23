42228ee79912b63f3fa24d0f37db8fef019eaade
Mathéo Delvert <115637444+Matheo-dlvt@users.noreply.github.com>
2025-04-24 01:35:41 +0200

    fix url

--- mobile/src/screens/auth/LoginScreen.tsx
+++ mobile/src/screens/auth/LoginScreen.tsx
@@ -21,7 +21,7 @@ const LoginScreen: React.FC = () => {
 
     try {
       // Exemple d'appel API de login
-      const response = await fetch("http://127.0.0.1:8000/api/login", {
+      const response = await fetch("http://127.0.0.1:8000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- mobile/src/screens/auth/RegisterScreen.tsx
+++ mobile/src/screens/auth/RegisterScreen.tsx
@@ -29,10 +29,10 @@ const RegisterScreen: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/register", {
+      const response = await fetch("http://127.0.0.1:8000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ firstName, lastName, email, password, confirmPassword, role: false }),
+        body: JSON.stringify({ firstName, lastName, email, password, confirmPassword}),
       });
 
       if (!response.ok) throw new Error("Erreur lors de l'inscription.");
